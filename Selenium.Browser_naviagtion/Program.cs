f941a0d1d58793078bf9515455d904468ee17a9a
Demchenko Kirill <thegalaxydias@gmail.com>
2021-10-04 20:14:51 +0300

    Добавьте файлы проекта.

---
+++ Selenium.Browser_naviagtion/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Selenium.Browser_naviagtion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://itechcraft.com/");
+            driver.Navigate().GoToUrl("https://itechcraft.com/about-it-craft/");
+            driver.Navigate().Back();
+            driver.Close();
+        }
+    }
+}
